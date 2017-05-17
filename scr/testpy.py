a85b3f578240c279c427b59c71f49a3c5985f3ae
SeanBeseler <seanwbeseler@gmail.com>
2017-05-17 13:01:31 -0700

    Sean's second commit

--- scr/testpy.py
+++ scr/testpy.py
@@ -5,22 +5,24 @@ def trigrams(text = 'temp.txt'):
     word2 = ''
     temp = ''
     fi = io.open(text, encoding ='utf-8')
-    print('hi')
     for line in fi:
         for word in line.split():
-            print('hi')
             if word1 == '':
                 word1 = word
             elif word2 == '':
                 word2 = word
             else:
-                temp = word1 + word2
-                if dictionary.has_key(temp):
-                    dictionary[tmep] = dictionary[temp].append(word)
+                temp = word1 +' '+ word2
+                if  temp in dictionary:
+                    print(dictionary[temp])
+                    ltemp = dictionary[temp]
+                    ltemp.appened(word)
+                    print(ltemp)
+                    dictionary[temp] = dictionary[temp].append(word)
                 else:
                     dictionary[temp] = [word]
-            word1 = word2
-            word2 = word
+                word1 = word2
+                word2 = word
     print(dictionary.items())
 trigrams()
                 
