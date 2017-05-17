ade025450924a7e2e382d7254cfc1f7c7a554877
James <jjfeore>
2017-05-17 14:54:10 -0700

    Wrote test functions

---
+++ src/test_trigrams.py
@@ -0,0 +1,28 @@
+"""Tests for trigrams.py."""
+
+
+import pytest
+
+
+DICT_PARAMS = [
+    ('and was', ['hot', 'shown']),
+    ('he was', ['employing']),
+    ('He was', ['at', 'pacing']),
+    ('his hands', ['clasped'])
+]
+
+
+@pytest.mark.parametrize('word_pair, result_list', DICT_PARAMS)
+def test_build_dict(word_pair, result_list):
+    """."""
+    from trigrams import build_dict
+    from trigrams import dictionary
+    build_dict()
+    assert dictionary[word_pair] == result_list
+
+
+@pytest.mark.parametrize('word_pair, result_list', DICT_PARAMS)
+def test_choose_third(word_pair, result_list):
+    """."""
+    from trigrams import choose_third
+    assert choose_third(word_pair) in result_list
